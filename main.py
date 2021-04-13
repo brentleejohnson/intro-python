1fac300b9f78a8d237cf20bba07d5a3bcceaf605
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-04-13 09:26:49 +0200

    Added main.py file.

---
+++ main.py
@@ -0,0 +1,14 @@
+# Task 1 Sample Solution
+print("Hello World!")
+
+# Task 2 Sample Solution
+radius = input("Please enter the radius: ")
+
+_PI = 3.14
+area = _PI * int(radius)**2
+print("The area of a circle is: " + str(area))
+
+
+# Calculate the circumference Task 3 Sample Solution
+circumference = 2 * _PI * int(radius)
+print("The circumference of a circle is: " + str(circumference))
